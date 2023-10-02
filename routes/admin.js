0b18595d6c7aaf7a90a0e361cb1d6dbf5d841215
Faiz <faizkhan8225@gmail.com>
2023-10-02 17:58:40 +0530

    Basic express js routing and setup

---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+const products = []
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({title: req.body.title})
+  res.redirect('/');
+});
+
+module.exports = {
+  routes: router,
+  products: products
+}
