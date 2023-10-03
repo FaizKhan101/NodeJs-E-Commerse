792fd11dc630e050a2bd4503da86c350ff313769
Faiz <faizkhan8225@gmail.com>
2023-10-03 21:13:53 +0530

    fetching only user related product

--- controllers/admin.js
+++ controllers/admin.js
@@ -36,13 +36,15 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then((products) => {
       res.render("admin/edit-product", {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product,
+        product: products[0],
       });
     })
     .catch((err) => {
@@ -71,7 +73,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
+    // Product.findAll()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
