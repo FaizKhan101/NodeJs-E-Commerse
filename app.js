2ed52c5ccdff8dc0fd4a560ee60b6cd9cff2f6cf
Faiz <faizkhan8225@gmail.com>
2023-10-03 12:45:44 +0530

    Ownword to MySQL database and remove fs

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin.routes');
-const shopRoutes = require('./routes/shop.routes.');
-const errorController = require("./controllers/error.controller")
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
