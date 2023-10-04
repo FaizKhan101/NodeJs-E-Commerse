db8461df2c94e1eedb7b7f1778938acc78378889
Faiz <faizkhan8225@gmail.com>
2023-10-04 11:50:04 +0530

    Add product into the cart item

--- controllers/shop.js
+++ controllers/shop.js
@@ -52,17 +52,43 @@ exports.getCart = (req, res, next) => {
           pageTitle: "Your Cart",
           products: products,
         });
-      }); 
+      });
     })
     .catch((err) => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      newQuantity = 1;
+      if (product) {
+        // ...
+      }
+      return Product.findByPk(prodId)
+        .then((product) => {
+          return fetchedCart.addProduct(product, {
+            through: { quantity: newQuantity },
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
