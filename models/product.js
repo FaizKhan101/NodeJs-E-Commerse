b741a8d74b125eb14b799b1bfc358572da5c6dea
Faiz <faizkhan8225@gmail.com>
2023-10-07 14:14:48 +0530

    Fixing add product issue

--- models/product.js
+++ models/product.js
@@ -7,7 +7,9 @@ class Product {
     this.imageUrl = imageUrl;
     this.price = price;
     this.description = description;
-    this._id = new ObjectId(id);
+    if (id) {
+      this._id = new ObjectId(id);
+    }
   }
 
   save() {
