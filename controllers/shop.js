192d9b9606b992aa80c79a25e782a3df86e13586
Faiz <faizkhan8225@gmail.com>
2023-10-12 23:25:37 +0530

    add all product data in the order collection

--- controllers/shop.js
+++ controllers/shop.js
@@ -88,7 +88,7 @@ exports.postOrder = (req, res, next) => {
     // .execPopulate()
     .then((user) => {
       const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: i.productId }
+        return { quantity: i.quantity, product: { ...i.productId._doc }}
       });
       const order = new Order({
         user: {
