b527b1648e7d844b24d92dd4d32ec3a9be32e59c
Faiz <faizkhan8225@gmail.com>
2023-10-13 23:22:00 +0530

    Add isLoggedIn demo Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req.get('Cookie').split('=')[1];
     res.render("auth/login", {
         pageTitle: "Login",
-        path: "login"
+        path: "login",
+        isAuthenticated: isLoggedIn
     })
+}
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true')
+    res.redirect("/")
 }
